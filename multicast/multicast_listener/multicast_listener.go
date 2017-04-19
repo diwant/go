f3c5305feb5072f1bf700e56e83c505f328ec8bc
Diwant Vaidya <dvaidya@enova.com>
2017-04-19 10:50:38 -0500

    Wrote speaker and listener to write to screen helpful messages that convey what is going on

--- multicast/multicast_listener/multicast_listener.go
+++ multicast/multicast_listener/multicast_listener.go
@@ -14,6 +14,8 @@ const servAddr string = "224.0.0.1:9876"
 const maxReadSize int = 8192
 
 func main() {
+	log.Println("Welcome to Multicast Listen-o-Tron 2.0")
+	log.Println("------------------------------------")
 	listenForMulticast(servAddr, castHandler)
 }
 
@@ -23,12 +25,24 @@ func castHandler(a *net.UDPAddr, i int, b []byte) {
 }
 
 func listenForMulticast(address string, handle func(*net.UDPAddr, int, []byte)) {
+
+	log.Println("...resolving address for listening on UDP at", address)
 	addr, err := net.ResolveUDPAddr("udp", address)
 	if err != nil {
 		log.Fatal("Could not resolve address", err)
 	}
+
+	log.Println("...initializing listen for multicast udp at", addr)
 	l, err := net.ListenMulticastUDP("udp", nil, addr)
+
+	log.Println("...setting read buffer max to", maxReadSize)
 	l.SetReadBuffer(maxReadSize)
+
+	log.Println("...setup complete!")
+
+	log.Println("...listening...")
+	log.Println("------------------------------------")
+	log.Println("")
 	for {
 		b := make([]byte, maxReadSize)
 		i, srcAddr, err := l.ReadFromUDP(b)
--- multicast/multicast_speaker/multicast_speaker.go
+++ multicast/multicast_speaker/multicast_speaker.go
@@ -5,7 +5,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"log"
 	"net"
 	"time"
@@ -15,17 +14,44 @@ const servAddr string = "224.0.0.1:9876"
 const maxReadSize int = 8192
 
 func main() {
-	go ping(servAddr)
+	log.Println("Speakbot 2017 Activated.  Initializing...")
+	log.Println("------------------------------------")
+	ping(servAddr)
 }
 
 func ping(address string) {
+	speakThis := "beep"
+
+	log.Println("[x] Confirm UDP address resolvable, address is", address)
 	addr, err := net.ResolveUDPAddr("udp", address)
 	if err != nil {
 		log.Fatal("Could not resolve address", err)
 	}
+
+	log.Println("[x] Dialing UDP address", addr)
 	c, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal("Could not Dial UDP address", err)
+	}
+
+	log.Println("[x] Connection success!")
+	log.Println("[x] Commencing speech transmission")
+	log.Println("------------------------------------")
+	log.Println("")
 	for {
-		c.Write([]byte("hum"))
+
+		log.Println("Saying", speakThis)
+		c.Write([]byte(speakThis))
+
+		// Change to the next word to speak
+		switch speakThis {
+		case "beep":
+			speakThis = "boop"
+		case "boop":
+			speakThis = "beep"
+		}
+
+		// Wait a second to say the next thing
 		time.Sleep(1 * time.Second)
 	}
 }
